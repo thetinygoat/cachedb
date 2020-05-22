a295cda629a0886e920097413be8c084e82cbbe6
Sachin Saini <iamsainisachin@gmail.com>
2020-05-22 18:18:48 +0530

    add travis yml

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var globalCacheObject = make(map[string]Cache)
-	globalCacheObject, _ = create("key1", "value1", 15, globalCacheObject)
-	globalCacheObject, _ = create("key2", "value2", 20, globalCacheObject)
+	var globalCacheObject = make(map[string]Cache) // global cache ref
+	globalCacheObject = set("key1", "value1", 15, globalCacheObject)
+	globalCacheObject = set("key2", "value2", 20, globalCacheObject)
+	fmt.Println(globalCacheObject)
+	value, err := get("key2", globalCacheObject)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(value)
+	}
 	fmt.Println(globalCacheObject)
 }
