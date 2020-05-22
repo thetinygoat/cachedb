dadccd81dc7ea892bf01836efa34d41e624ff575
Sachin Saini <iamsainisachin@gmail.com>
2020-05-22 22:23:58 +0530

    add initializers

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var globalCacheObject = make(map[string]Cache) // global cache ref
-	globalCacheObject = set("key1", "value1", 15, globalCacheObject)
-	fmt.Println(globalCacheObject)
-	flush(globalCacheObject)
-	fmt.Println(globalCacheObject)
-
+	CacheRef := GlobalCacheRef{}
+	Router := RouterConfig{}
+	CacheRef.intializeGlobalCache()
+	Router.initializeRouter(CacheRef)
+	Router.registerRoutes()
 }
