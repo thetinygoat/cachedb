1afff63aaa99db99c3571ae210463effa92112e8
Sachin Saini <iamsainisachin@gmail.com>
2020-05-22 18:07:42 +0530

    add get and del operations

--- store.go
+++ store.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"errors"
 	"time"
+
+	"github.com/google/uuid"
+)
+
+// errors
+const (
+	KeyDoesNotExist = "KEY_DOES_NOT_EXIST_ERROR"
+	KeyExpired      = "KEY_EXPIRED_ERROR"
 )
 
 // Cache : holds cache signature
@@ -13,13 +22,34 @@ type Cache struct {
 	TTL        time.Duration
 }
 
-func create(key string, value string, ttl int, globalCacheObject map[string]Cache) (map[string]Cache, error) {
+// insert data in the store
+func set(key string, value string, ttl int, globalCacheObject map[string]Cache) map[string]Cache {
 	globalCacheObject[key] = Cache{
 		Data:       value,
-		Identifier: "id",
+		Identifier: uuid.New().String(),
 		CreatedAt:  time.Now(),
 		ExpireAt:   time.Now().Add(time.Duration(ttl) * time.Second),
 		TTL:        time.Duration(ttl) * time.Second,
 	}
-	return globalCacheObject, nil
+	return globalCacheObject
+}
+
+func get(key string, globalCacheObject map[string]Cache) (string, error) {
+	if cacheObject, ok := globalCacheObject[key]; ok {
+		if time.Now().Sub(cacheObject.ExpireAt) > 0 {
+			delete(globalCacheObject, key)
+			return KeyExpired, errors.New(KeyExpired)
+		}
+		return cacheObject.Data, nil
+	}
+	return KeyDoesNotExist, errors.New(KeyDoesNotExist)
+
+}
+
+func del(key string, globalCacheObject map[string]Cache) error {
+	if _, ok := globalCacheObject[key]; ok {
+		delete(globalCacheObject, key)
+		return nil
+	}
+	return errors.New(KeyDoesNotExist)
 }
