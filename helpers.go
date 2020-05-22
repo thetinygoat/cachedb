36955793ffda9a16227b5ba9b92e6786504eba0c
Sachin Saini <iamsainisachin@gmail.com>
2020-05-22 22:23:24 +0530

    add json response builder

--- helpers.go
+++ helpers.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, res Response) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&res)
+}
