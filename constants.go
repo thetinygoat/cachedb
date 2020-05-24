230fa8cbb27bee19cd816ebca1cd4649d393a941
DarioSindicic <dario.sindicic@gmail.com>
2020-05-24 11:09:43 +0200

    defined Route structs

--- constants.go
+++ constants.go
@@ -1,5 +1,12 @@
 package main
 
+import "net/url"
+
+type Route struct {
+	Path      string
+	variables map[string]string
+}
+
 // Constants used throughout the project
 const (
 	MalformedParams = "MALFORMED_PARAMS"
@@ -7,5 +14,41 @@ const (
 	KeyExpired      = "KEY_EXPIRED_ERROR"
 	OK              = "OK"
 
-    DefaultPort     = "9898"
+	DefaultPort = "9898"
 )
+
+var GetRoute = Route{
+	Path: "/get",
+	variables: map[string]string{
+		"key": "key",
+	},
+}
+
+var SetRoute = Route{
+	Path: "/set",
+	variables: map[string]string{
+		"key":    "key",
+		"value":  "value",
+		"ttlRaw": "ttl",
+	},
+}
+
+var DelRoute = Route{
+	Path: "/del",
+	variables: map[string]string{
+		"key": "key",
+	},
+}
+
+var FlushRoute = Route{
+	Path:      "/flush",
+	variables: map[string]string{},
+}
+
+func (route Route) getVars(values url.Values) map[string]string {
+	result := make(map[string]string)
+	for k, v := range route.variables {
+		result[k] = values.Get(v)
+	}
+	return result
+}
