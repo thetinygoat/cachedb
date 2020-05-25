60a40f90106ec38f67306f625683afc6e4e9aa53
Sachin Saini <iamsainisachin@gmail.com>
2020-05-26 03:20:29 +0530

    implement list data structure using doubly linked lists

---
+++ list.go
@@ -0,0 +1,135 @@
+// Copyright (C) 2020  Sachin Saini
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// Constants
+const (
+	listEmpty = "LIST_EMPTY"
+)
+
+// ListNode -> Node struct
+type ListNode struct {
+	Data string
+	Next *ListNode
+	Prev *ListNode
+}
+
+// List -> List struct
+type List struct {
+	Name  string
+	Head  *ListNode
+	Tail  *ListNode
+	Size  int
+	mutex sync.Mutex
+}
+
+func (l *List) rpush(data string) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.Head == nil {
+		l.Head = &ListNode{Data: data, Next: nil, Prev: nil}
+		l.Tail = l.Head
+	} else {
+		l.Tail.Next = &ListNode{Data: data, Next: nil, Prev: l.Tail}
+		l.Tail = l.Tail.Next
+	}
+	l.Size++
+	return nil
+}
+
+func (l *List) lpush(data string) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.Head == nil {
+		l.Head = &ListNode{Data: data, Next: nil, Prev: nil}
+		l.Tail = l.Head
+	} else {
+		node := &ListNode{Data: data, Next: l.Head, Prev: nil}
+		l.Head = node
+	}
+	l.Size++
+	return nil
+}
+
+func (l *List) rpop() (string, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.Size <= 0 {
+		return "", errors.New(listEmpty)
+	}
+	rdata := l.Tail.Data
+	l.Tail = l.Tail.Prev
+	if l.Tail != nil {
+		l.Tail.Next = nil
+	}
+	if l.Size == 1 {
+		l.Head = nil
+		l.Tail = nil
+	}
+	l.Size--
+	return rdata, nil
+}
+
+func (l *List) lpop() (string, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.Head == nil {
+		return "", errors.New(listEmpty)
+	}
+	rdata := l.Head.Data
+	l.Head = l.Head.Next
+	if l.Head != nil {
+		l.Head.Prev = nil
+	}
+	if l.Size == 1 {
+		l.Head = nil
+		l.Tail = nil
+	}
+	l.Size--
+	return rdata, nil
+}
+
+func (l *List) lrange(start int, stop int) int {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if start < 0 {
+		return 1
+	}
+	if stop < 0 {
+		stop = l.Size
+	}
+	if stop > l.Size {
+		stop = l.Size
+	}
+	idx := 0
+	startRef := l.Head
+	for idx != start {
+		startRef = startRef.Next
+		idx++
+	}
+	for start < stop {
+		fmt.Println(startRef.Data)
+		startRef = startRef.Next
+		start++
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	list := List{Name: "mylist"}
+	list.rpush("1")
+	list.rpush("2")
+	list.rpush("3")
+	list.rpush("4")
+	list.rpush("5")
+	list.rpush("6")
+	fmt.Println(list.lpop())
+	fmt.Println(list.lpop())
+	fmt.Println(list.lpop())
+	fmt.Println(list.lpop())
+	fmt.Println(list.lpop())
+	fmt.Println(list.lpop())
+	list.lrange(0, 999)
 }
