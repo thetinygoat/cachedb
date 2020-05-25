e3666f8df838103a50f4e8c454bc81fad4a4957f
Sachin Saini <iamsainisachin@gmail.com>
2020-05-26 02:22:37 +0530

    rethink db architecture

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/thetinygoat/cachedb
 
 go 1.14
-
-require (
-	github.com/go-chi/chi v4.1.1+incompatible
-	github.com/google/uuid v1.1.1
-)
--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
 func main() {
-	CacheRef := GlobalCacheRef{}
-	Router := RouterConfig{}
-	CacheRef.intializeGlobalCache()
-	Router.initializeRouter(CacheRef)
-	Router.registerRoutes()
+
 }
