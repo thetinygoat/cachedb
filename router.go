578b773b884b14fbaf68f08d7bb533849960574f
Sachin Saini <iamsainisachin@gmail.com>
2020-05-27 01:49:29 +0530

    add set routes

--- router.go
+++ router.go
@@ -23,93 +23,124 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-const (
-	ok = "OK"
-)
-
 // Router -> holds router config
-type Router struct{
+type Router struct {
 	router *httprouter.Router
 }
 
-func rpushHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func rpushHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	name := ps.ByName("name")
 	if _, ok := LMap[name]; !ok {
 		LMap[name] = &List{Name: name}
 	}
 	data := r.URL.Query().Get("data")
 	err := LMap[name].rpush(data)
-	if(err != nil){
-		fmt.Fprint(w,err.Error())
-	}else{
-		fmt.Fprint(w,ok)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	} else {
+		fmt.Fprint(w, ok)
 	}
 }
 
-func rpopHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func rpopHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	name := ps.ByName("name")
 	if _, ok := LMap[name]; !ok {
 		LMap[name] = &List{Name: name}
 	}
-	data,err := LMap[name].rpop()
-	if(err != nil){
-		fmt.Fprint(w,err.Error())
-	}else{
-		fmt.Fprint(w,data)
+	data, err := LMap[name].rpop()
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	} else {
+		fmt.Fprint(w, data)
 	}
 }
 
-func lpushHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func lpushHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	name := ps.ByName("name")
 	if _, ok := LMap[name]; !ok {
 		LMap[name] = &List{Name: name}
 	}
 	data := r.URL.Query().Get("data")
 	err := LMap[name].lpush(data)
-	if(err != nil){
-		fmt.Fprint(w,err.Error())
-	}else{
-		fmt.Fprint(w,ok)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	} else {
+		fmt.Fprint(w, ok)
 	}
 }
 
-func lpopHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func lpopHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	name := ps.ByName("name")
 	if _, ok := LMap[name]; !ok {
 		LMap[name] = &List{Name: name}
 	}
-	data,err := LMap[name].lpop()
-	if(err != nil){
-		fmt.Fprint(w,err.Error())
-	}else{
-		fmt.Fprint(w,data)
+	data, err := LMap[name].lpop()
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	} else {
+		fmt.Fprint(w, data)
 	}
 }
 
-func lrangeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func lrangeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	name := ps.ByName("name")
 	if _, ok := LMap[name]; !ok {
 		LMap[name] = &List{Name: name}
 	}
 	startRaw := r.URL.Query().Get("start")
 	stopRaw := r.URL.Query().Get("stop")
-	start,err := strconv.Atoi(startRaw)
+	start, err := strconv.Atoi(startRaw)
 	stop, err := strconv.Atoi(stopRaw)
-	data,err := LMap[name].lrange(start, stop)
-	if(err != nil){
-		fmt.Fprint(w,err.Error())
-	}else{
-		fmt.Println(data)
-		fmt.Fprint(w,data)
+	data, err := LMap[name].lrange(start, stop)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	} else {
+		fmt.Fprint(w, data)
 	}
 }
 
-func (R * Router) initializeRouter(){
+func setHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	if _, ok := SMap[key]; !ok {
+		SMap[key] = &Set{}
+	}
+	value := r.URL.Query().Get("value")
+	ttlRaw := r.URL.Query().Get("ttl")
+	ttl, _ := strconv.Atoi(ttlRaw)
+	SMap[key].set(value, ttl)
+	fmt.Fprint(w, ok)
+}
+func getHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	if _, ok := SMap[key]; !ok {
+		fmt.Fprint(w, nilString)
+		return
+	}
+	data, err := SMap[key].get()
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		delete(SMap, key)
+	} else {
+		fmt.Fprint(w, data)
+	}
+}
+
+func delHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	delete(SMap, key)
+	fmt.Fprint(w, ok)
+
+}
+
+func (R *Router) initializeRouter() {
 	R.router = httprouter.New()
-	R.router.POST("/list/rpush/:name", rpushHandler)
-	R.router.POST("/list/rpop/:name", rpopHandler)
-	R.router.POST("/list/lpush/:name", lpushHandler)
-	R.router.POST("/list/lpop/:name", lpopHandler)
-	R.router.GET("/list/lrange/:name", lrangeHandler)
+	R.router.POST("/lists/rpush/:name", rpushHandler)
+	R.router.POST("/lists/rpop/:name", rpopHandler)
+	R.router.POST("/lists/lpush/:name", lpushHandler)
+	R.router.POST("/lists/lpop/:name", lpopHandler)
+	R.router.GET("/lists/lrange/:name", lrangeHandler)
+	R.router.POST("/sets/set/:key", setHandler)
+	R.router.GET("/sets/get/:key", getHandler)
+	R.router.POST("/sets/del/:key", delHandler)
 	log.Fatal(http.ListenAndServe(":9898", R.router))
 }
