f634ad55194eb87d12ba05d5b0109aed75b4b1c1
Sachin Saini <iamsainisachin@gmail.com>
2020-06-15 04:50:44 +0530

    test lru cache using Cache interface

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	ReadConfig()
+	lru := &LRU{list: New(), cache: make(map[string]*element), capacity: MaxMemory}
+	value, status := Set(lru, "sachin", "i am sachin", 2)
+	fmt.Println(value, status)
+	val, _ := Get(lru, "sachin")
+	fmt.Println(val, lru.size)
+	time.Sleep(4 * time.Second)
+	val, _ = Get(lru, "sachin")
+	fmt.Println(val, lru.size)
+	fmt.Println(lru.cache, lru.capacity)
 
 }
